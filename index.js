61f1d6967e609dd35d6c38530bad21747d796ce6
Jake Sower <jake@jakesower.com>
2020-11-28 13:37:54 -0500

    more experimenting

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 const sourceDir = __dirname;
 const targetDir = process.cwd();
 
-fs.copyFileSync(path.join(sourceDir, '/', 'moo.txt'), path.join(targetDir, '/', 'moo.moo'));
+const sourcePath = pathStr => path.join(sourceDir, '/', ...pathStr.split('/'));
+const targetPath = pathStr => path.join(sourceDir, '/', ...pathStr.split('/'));
+
+// fs.ensureDirSync(targetPath('pg-tests'));
+// fs.ensureDirSync(targetPath('pg-tests/schemata'));
+
+fs.copyFileSync(sourcePath('tests'), targetPath('pg-tests'));
