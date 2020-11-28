b32080aaee6d97633f7ba5db5e40b371f35871d3
Jake Sower <jake@jakesower.com>
2020-11-28 14:01:24 -0500

    more experimenting

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const path = require('path');
 
 const sourceDir = __dirname;
 const targetDir = process.cwd();
-console.log({ sourceDir, targetDir });
 
 const sourcePath = pathStr => path.join(sourceDir, '/', ...pathStr.split('/'));
 const targetPath = pathStr => path.join(targetDir, '/', ...pathStr.split('/'));
 
-// fs.copyFileSync(sourcePath('tests'), targetPath('pg-tests'));
+fs.copyFileSync(sourcePath('tests'), targetPath('pg-tests'));
